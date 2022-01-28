bedfc3c8d4dba97ddaf1dd9e45c5bdfbc6f92d75
Artem <free44@yandex.ru>
2022-01-29 00:20:47 +0300

    Решение задачи массива

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -4,7 +4,15 @@ var result = new string[array.Length];
 var realSize = 0;
 foreach (var value in array)
 
-
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.Write(string.Join(" ", result, 0, realSize));
 
 
 
